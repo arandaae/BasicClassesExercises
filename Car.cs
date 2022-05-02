ddc5e6cf61c73c940c2f16ce890c7cc97d6de7f2
Armando Aranda <arandaae19@gmail.com>
2022-05-02 11:22:16 -0500

    First Commit

---
+++ Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExercise
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+
+        public void NewCar()
+        {
+            Console.WriteLine(Make);
+            Console.WriteLine(Model);
+            Console.WriteLine(Year);
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+
+            car1.Make = "Toyota";
+            car1.Model = "Camry";
+            car1.Year = "2022";
+
+            car1.NewCar();
+
+
+        }
+    }
+}
